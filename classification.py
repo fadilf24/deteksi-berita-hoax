7a874f6ffc536b84ff302a7b58095a1b13cdccdf
Fadil Farhan Ridwan <98074501+fadilf24@users.noreply.github.com>
2025-08-12 13:23:16 +0700

    Update classification.py

--- classification.py
+++ classification.py
@@ -9,7 +9,7 @@ from sklearn.base import ClassifierMixin
 def split_data(
     X: Any, 
     y: Any, 
-    test_size: float = 0.2, 
+    test_size: float = 0.4, 
     random_state: int = 42
 ) -> Tuple[Any, Any, Any, Any]:
     """
@@ -87,6 +87,7 @@ def prediction_distribution(
         for label, count in zip(unique, counts)
     }
     return percentages
+
 
 
 
